827138d4164830751c09156e73c987bb34c2ef84
AbdulQadirJillani <abdulqadir786687786@gmail.com>
2025-05-02 20:28:17 +0500

    version 1(compiled + login)

---
+++ app/(PastPapers)/PastPapers/[...QA]/page.tsx
@@ -0,0 +1,33 @@
+import Format from "@/app/_QAFormat/Format"
+import { redirect } from "next/navigation"
+
+type Props = {
+  QA: string[]
+}
+
+type Data = {
+  id: number,
+  info: string|string[],
+  question: string,
+  answers: { option: string, bool: boolean }[]
+}[]
+
+const page = async({ params }: { params: Props }) => {
+  let data: Data
+  try {
+    const { QA } = await params
+    const annual = QA[0]
+    const[module, year] = QA[1].split('-')
+    const promisedData = await import(`../../../../Database/PastPapers/${annual}/${module}/${module}-${year}.jsx`)
+    data = await promisedData.default
+  }
+  catch {
+    redirect("/")
+  }
+
+  return (
+    <Format data={data}/>
+  )
+}
+
+export default page
