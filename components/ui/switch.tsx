4319b1e04059509c3b3b608c38e6680f9ce72993
AbdulQadirJillani <abdulqadir786687786@gmail.com>
2025-05-28 17:18:25 +0500

    version 1(transition fixed + toggle improved)

--- components/ui/switch.tsx
+++ components/ui/switch.tsx
@@ -4,6 +4,8 @@ import * as React from "react"
 import * as SwitchPrimitive from "@radix-ui/react-switch"
 
 import { cn } from "@/lib/utils"
+import Sun from "@/app/_NavBar/Sun"
+import Moon from "@/app/_NavBar/Moon"
 
 function Switch({
   className,
@@ -13,7 +15,7 @@ function Switch({
     <SwitchPrimitive.Root
       data-slot="switch"
       className={cn(
-        "peer data-[state=checked]:bg-accent data-[state=unchecked]:bg-input focus-visible:border-ring focus-visible:ring-ring/50 dark:data-[state=unchecked]:bg-input/80 inline-flex h-[1.15rem] w-8 shrink-0 items-center rounded-full border border-transparent shadow-xs transition-all outline-none focus-visible:ring-[3px] disabled:cursor-not-allowed disabled:opacity-50",
+        "bg-background shadow-[rgba(0,0,0,0.2)] dark:shadow-accent shadow-[inset_-1px_0px_7px] dark:shadow-[inset_1px_0px_7px] inline-flex h-7 w-12 shrink-0 items-center rounded-full focus-visible:border-ring focus-visible:ring-ring/50 transition-all outline-none focus-visible:ring-[3px] disabled:cursor-not-allowed disabled:opacity-50",
         className
       )}
       {...props}
@@ -21,9 +23,13 @@ function Switch({
       <SwitchPrimitive.Thumb
         data-slot="switch-thumb"
         className={cn(
-          "bg-background dark:data-[state=unchecked]:bg-foreground dark:data-[state=checked]:bg-primary-foreground pointer-events-none block size-4 rounded-full ring-0 transition-transform data-[state=checked]:translate-x-[calc(100%-2px)] data-[state=unchecked]:translate-x-0"
+          "bg-background shadow-[rgba(0,0,0,0.3)] dark:shadow-[rgba(248,250,252,0.3)] shadow-[inset_1px_0px_7px] pointer-events-none block size-6 p-1 mx-[1.5px] rounded-full transition-transform duration-300 data-[state=checked]:translate-x-[calc(100%-3px)] data-[state=unchecked]:translate-x-0"
         )}
-      />
+      >
+        {
+          props.checked ? <Moon /> : <Sun />
+        }
+      </SwitchPrimitive.Thumb>
     </SwitchPrimitive.Root>
   )
 }
